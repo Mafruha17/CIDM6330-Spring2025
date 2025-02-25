758738fde8aee10134b58d077561277a9217eb05
Mafruha Chowdhury <Mafruha.Chowdhury@gmail.com>
2025-02-24 19:57:01 -0600

    c

--- Assignment 2/main.py
+++ Assignment 2/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from routers import patient_routes
+from utilsclasses.custom_logger import log_info
 
 app = FastAPI()
 
@@ -7,4 +8,5 @@ app.include_router(patient_routes.router)
 
 @app.get("/")
 def home():
+    log_info("Home endpoint was accessed.")
     return {"message": "Welcome to the FastAPI App with SQLite!"}
