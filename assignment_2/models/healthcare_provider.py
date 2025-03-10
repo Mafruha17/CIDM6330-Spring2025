2fd66b3a8fe29a6c9eec90cf39c942c41baea20e
Mafruha Chowdhury <Mafruha.Chowdhury@gmail.com>
2025-03-09 21:30:50 -0500

    git

--- assignment_2/models/healthcare_provider.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from database.connection import Base
-
-class HealthcareProvider(Base):
-    __tablename__ = "healthcare_providers"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    specialization = Column(String, nullable=True)
--- assignment_2/routes/device_routes.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.get("/devices/")
-def get_devices():
-    return {"message": "List of devices"}
